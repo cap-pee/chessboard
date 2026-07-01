f477c6a6ce06437806e534d43164f122e03f07fa
cap-pee <abdurrafeyamir15@gmail.com>
2026-07-02 03:24:29 +0500

    knight, rook, bishop, queen movement logic done.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 
-board = [['']* 8  for i in range(8)]
+board = [[None]* 8  for i in range(8)]
 for i in range(8):
     print(f'{board[i]}')
 
@@ -36,10 +36,11 @@ class Knight(Piece):
         (2, 1)
         ]
 
-    def getLegalMoves(self, row, col):
+    def getLegalMoves(self, row, col, board):
         # current possible moves
 
         moves = []
+        capturable_pieces = []
 
         for dr, dc in self.knight_moves:
             new_row = row + dr
@@ -48,9 +49,25 @@ class Knight(Piece):
             # check if it's on the board
             if new_row >= 0 and new_row < 8:
                 if new_col >= 0 and new_col < 8:
-                    moves.append((new_row, new_col))
-        
-        return moves
+                    destination = board[new_row][new_col]
+
+                    # check if it's your own piece or not
+                    if isinstance(destination, Piece):
+                        if destination.getColor() == self.getColor():
+                            print('same team')
+                            continue
+                        else:
+                            moves.append((new_row, new_col))
+         
+                            # add to another list if there's a piece you can capture
+                            capturable_pieces.append((new_row, new_col))
+         
+                    # empty square
+                    else:
+                        moves.append((new_row, new_col))
+
+
+        return moves, capturable_pieces
     
 
 class Pawn(Piece):
@@ -79,15 +96,49 @@ class Rook(Piece):
         super().__init__(color, 'R')
 
 
-    rook_moves = [
+    rook_directions = [
         (-1, 0), 
         (1, 0),
         (0, -1),
         (0, 1)
     ]
 
-    def getLegalMoves(self, row, col):
-        pass
+    def getLegalMoves(self, row, col, board):
+        # current possible moves
+
+        moves = []
+        capturable_pieces = []
+
+
+        for dr, dc in self.rook_directions:
+            sqs_moved = 1
+            
+            # loop to keep checking further squares
+            while True:
+                new_row = row + (dr * sqs_moved)
+                new_col = col + (dc * sqs_moved)
+            
+                if new_row >= 0 and new_row < 8:
+                    if new_col >= 0 and new_col < 8:
+                        destination = board[new_row][new_col]
+                        if isinstance(destination, Piece):
+                            if destination.getColor() == self.getColor():
+                                print('same team')
+                            else:
+                                moves.append((new_row, new_col))
+                                capturable_pieces.append((new_row, new_col))
+                            # break loop if there's any piece in the way
+                            break 
+                        else:
+                            moves.append((new_row, new_col))
+                            sqs_moved += 1
+                            # keep checking if square is empty
+                            continue
+                # break loop if destination not on the board
+                break
+
+        return moves, capturable_pieces
+                            
 
 
 class Bishop(Piece):
@@ -95,15 +146,43 @@ class Bishop(Piece):
     def __init__(self, color):
         super().__init__(color, 'B')
 
-    bishop_moves = [
+    bishop_directions = [
         (-1, -1),
         (-1, 1),
         (1, -1),
         (1, 1)
     ]
 
-    def getLegalMoves(self, row, col):
-        pass
+    def getLegalMoves(self, row, col, board):
+
+        moves = []
+        capturable_pieces = []
+
+
+        for dr, dc in self.bishop_directions:
+            sqs_moved = 1
+
+            while True:
+                new_row = row + (dr * sqs_moved)
+                new_col = col + (dc * sqs_moved)
+
+                if new_row >= 0 and new_row < 8:
+                    if new_col >= 0 and new_col < 8:
+                        destination = board[new_row][new_col]
+                        if isinstance(destination, Piece):
+                            if destination.getColor() == self.getColor():
+                                print('same team')
+                            else:
+                                moves.append((new_row, new_col))
+                                capturable_pieces.append((new_row, new_col))
+                            break
+                        else:
+                            moves.append((new_row, new_col))
+                            sqs_moved += 1
+                            continue
+                break
+
+        return moves, capturable_pieces
 
 
 class Queen(Piece):
@@ -111,7 +190,7 @@ class Queen(Piece):
     def __init__(self, color):
         super().__init__(color, 'Q')
 
-    queen_moves = [
+    queen_directions = [
         (-1, 0),
         (-1, -1),
         (-1, 1),
@@ -122,8 +201,37 @@ class Queen(Piece):
         (1, 1)
     ]
 
-    def getLegalMoves(self, row, col):
-        pass
+    def getLegalMoves(self, row, col, board):
+        
+
+        moves = []
+        capturable_pieces = []
+
+
+        for dr, dc in self.queen_directions:
+            sqs_moved = 1
+
+            while True:
+                new_row = row + (dr * sqs_moved)
+                new_col = col + (dc * sqs_moved)
+
+                if new_row >= 0 and new_row < 8:
+                    if new_col >= 0 and new_col < 8:
+                        destination = board[new_row][new_col]
+                        if isinstance(destination, Piece):
+                            if destination.getColor() == self.getColor():
+                                print('same team')
+                            else:
+                                moves.append((new_row, new_col))
+                                capturable_pieces.append((new_row, new_col))
+                            break
+                        else:
+                            moves.append((new_row, new_col))
+                            sqs_moved += 1
+                            continue
+                break
+
+        return moves, capturable_pieces
 
 
 class King(Piece):
