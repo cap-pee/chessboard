5155db130d2ee8cf4b83e555799a38d374e04e79
cap-pee <abdurrafeyamir15@gmail.com>
2026-07-14 20:53:01 +0500

    function that assigns scores to every piece and does a calculation to see which team is in the better position

---
+++ evaluation.py
@@ -0,0 +1,28 @@
+from main import Board
+
+
+
+values = {
+    'P': 1,
+    'Kn': 3,
+    'B': 3,
+    'R': 5,
+    'Q': 9,
+    'K': 1000000
+}
+
+def evaluate(board):
+    score = 0
+    for row in range(8):
+        for col in range(8):
+            if not board.isEmpty(row, col):
+                piece = board.getPiece(row, col)
+                pieceType = piece.getType()
+                color = piece.getColor()
+                val = values[pieceType]
+                if color == 'white':
+                    score += val
+                else:
+                    score -= val
+    
+    return score
